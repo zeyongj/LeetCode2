22b0812cf60c4fd2294efae4f66bab81eeea0efd
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2026-03-05 01:41:07 -0800

    Time: 4 ms (92.63%), Space: 19.4 MB (22.11%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minOperations(self, s: str) -> int:
+        count, n = 0, len(s)
+        for i in range(n):
+            count += (ord(s[i]) ^ i) & 1
+            
+        return min(count, n - count)
