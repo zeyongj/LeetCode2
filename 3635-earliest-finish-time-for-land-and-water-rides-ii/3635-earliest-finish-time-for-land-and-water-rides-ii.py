20c04195529b8605fa436a278c6dc540f16e4f29
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2026-06-03 01:17:35 -0700

    Time: 95 ms (94.55%), Space: 34.8 MB (50.91%) - LeetHub

---
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def earliestFinishTime(self, ls: List[int], ld: List[int], ws: List[int], wd: List[int]) -> int:
+        minLandEnd=min(s+d for s, d in zip(ls, ld))
+        minWaterEnd=min(s+d for s,d in zip(ws, wd))
+        lw=min(max(minLandEnd, s)+d for s, d in zip(ws, wd))
+        wl=min(max(minWaterEnd, s)+d for s,d in zip(ls, ld))
+        return min(lw, wl)
+        
