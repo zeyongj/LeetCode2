bd2938ab462c07454a32918013beaab331cbe755
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2026-07-29 00:03:44 -0700

    Time: 23 ms (11.59%), Space: 19.4 MB (21.53%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,4 +1,10 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        return moves.count('L') == moves.count('R') and \
-               moves.count('U') == moves.count('D')
+        if len(moves) & 1: return False
+        x = y = 0
+
+        for c in moves:
+            y += (c == 'U') - (c == 'D')
+            x += (c == 'R') - (c == 'L')
+
+        return not x and not y
