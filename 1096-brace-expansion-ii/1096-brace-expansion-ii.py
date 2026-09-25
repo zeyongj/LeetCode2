b9865337bfc36699f26f169bf8058bf777b8d680
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2026-09-25 14:31:45 -0700

    Time: 19 ms (5.7%), Space: 20.1 MB (5.18%) - LeetHub

---
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def braceExpansionII(self, expression: str) -> list[str]:
+        ans = set()
+
+        def dfs(s):
+            r = s.find('}')
+
+            # No braces left
+            if r == -1:
+                ans.add(s)
+                return
+
+            # Find matching '{'
+            l = s.rfind('{', 0, r)
+
+            left = s[:l]
+            right = s[r + 1:]
+
+            # Content inside { }
+            inside = s[l + 1:r]
+
+            for part in inside.split(','):
+                dfs(left + part + right)
+
+        dfs(expression)
+        return sorted(ans)        
